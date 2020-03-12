8f0c52c998b6c5ab5d53c71432eba0d4f8520c3e
Fernando <47810697+TheHumphrey@users.noreply.github.com>
2020-03-12 08:47:45 -0300

    Criando os campos do componente "Login"
    - Criado os campos do componente de login;
    
    - Implementado os validator no login;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './views/login/login.component';
 import { CadastroComponent } from './views/cadastro/cadastro.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { CadastroComponent } from './views/cadastro/cadastro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatSliderModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { apiURL } from "../../../environments/environment";
+import { FormGroup, Validators, FormGroupDirective, FormControl, NgForm } from '@angular/forms';
+import { empty, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -7,9 +21,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
 
-  ngOnInit(): void {
+  matcher = new MyErrorStateMatcher();
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(40)
+  ])
+
+  constructor(private http: HttpClient) {
+
   }
 
+
+  ngOnInit(): void {
+
+  }
+
+  login() {
+    const email = this.emailFormControl.value;
+    const password = this.passwordFormControl.value;
+
+    if (email && password.length >= 8) {
+      this.http.post(`${apiURL}/auth`, { username: email, password: password }, { observe: "response" })
+        .pipe(catchError(err => {
+          return empty();
+        }))
+        .subscribe(res => console.log(res));
+    }
+  }
 }
