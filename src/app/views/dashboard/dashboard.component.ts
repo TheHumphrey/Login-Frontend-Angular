e222d08239acc3861b4fe537cc119881510725f0
Fernando <47810697+TheHumphrey@users.noreply.github.com>
2020-03-25 10:51:40 -0300

    Chart.js Update
    - Atualizando os graficos

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -89,8 +89,12 @@ export class DashboardComponent implements OnInit {
         ]
       },
       options: {
-        maintainAspectRatio: true,
-        
+        maintainAspectRatio: false,
+        layout: {
+          padding:{
+            bottom: 15
+          },
+        }
       }
     });
   }
@@ -100,25 +104,16 @@ export class DashboardComponent implements OnInit {
     var myChart = new Chart(element, {
       type: 'bar',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Em Dias', 'Atraso'],
         datasets: [{
-          label: '# of Votes',
           data: [12, 19, 3, 5, 2, 3],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.6)',
-            'rgba(54, 162, 235, 0.6)',
-            'rgba(255, 206, 86, 0.6)',
-            'rgba(75, 192, 192, 0.6)',
-            'rgba(153, 102, 255, 0.6)',
-            'rgba(255, 159, 64, 0.6)'
+            'rgba(41, 255, 73, .6)',
+            'rgba(255, 41, 41, .6)',
           ],
           borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
+            'rgba(41, 255, 73, 1)',
+            'rgba(255, 41, 41, 1)',
           ],
           borderWidth: 1
         }]
@@ -127,22 +122,23 @@ export class DashboardComponent implements OnInit {
         legend: {
           display: false
         },
-        maintainAspectRatio: false,
-        title: {
-          display: false,
-          text: 'Teste'
+        layout: {
+          padding:{
+            bottom: 3
+          },
         },
+        maintainAspectRatio: false,
         scales: {
           yAxes: [{
             stacked: true,
             gridLines: {
-              display: true,
+              display: false,
               color: "rgba(158, 158, 158, .6)"
             }
           }],
           xAxes: [{
             gridLines: {
-              display: false
+              display: true
             }
           }]
         }
